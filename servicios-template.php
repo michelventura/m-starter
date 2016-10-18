95cba51d367a189ec79603635454ff5d0f506abc
Michel <pmichven@gmail.com>
2016-10-18 21:10:00 +0000

    Fixed servicios page scrolling

---
+++ servicios-template.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+* Template Name: Servicios
+* Description: Template used for the services page
+*/
+
+remove_action('genesis_loop','genesis_do_loop');
+add_action('genesis_loop','custom_genesis_servicespage_loop');
+
+function custom_genesis_servicespage_loop() {
+?>
+
+<div class="usp-start">
+<h1>¿Cómo puedo aportar valor a tu negocio?</h1>
+<p>Te ofrezco soluciones que se acoplen a tu situación actual.<a href="http://michstart.dev/servicios/#soluciones" rel="m_PageScroll2id"> Selecciona y da clic en una de las siguientes</a> para conocer más.</p>
+</div>
+<div class="centrado">
+        <section class="mv-propuestas">
+            <article class="mv-propuesta propuesta-uno">
+                <a href="http://michstart.dev/servicios/#uno" rel="m_PageScroll2id" class="diy">
+                    <figure class="mv-thumbnail">
+                        <img src="http://michstart.dev/wp-content/uploads/one.png" alt="Propuesta uno">
+                    </figure>
+                    <div class="mv-propuesta-content">
+                        <h2>Hazlo tú solo, yo te enseño.</h2>
+        <p>Aquí yo te enseño a crear tu propia página web desde 0, lo único que necesitas, si quieres tener un dominio público, es un dominio y un servicio de hosting.</p>
+          </div>
+      </a>
+            </article>
+            <article class="mv-propuesta propuesta-dos">
+                <a href="http://michstart.dev/servicios/#dos" rel="m_PageScroll2id" class="diwm">
+                    <figure class="mv-thumbnail">
+                        <img src="http://michstart.dev/wp-content/uploads/two.png" alt="Propuesta dos">
+                    </figure>
+                    <div class="mv-propuesta-content">
+                        <h2>Te ayudo a hacerlo, tú le sigues.</h2>
+                        <p>Te ofrezco 2 soluciones distintas para que comiences a crear tu página web con una base sólida y entrenamiento para que sepas cómo crear más contenido de manera estratégica.</p>
+                    </div>
+                </a>
+            </article>
+            <article class="mv-propuesta propuesta-tres">
+                <a href="http://michstart.dev/servicios/#tres" rel="m_PageScroll2id" class="ldt">
+                    <figure class="mv-thumbnail">
+                        <img src="http://michstart.dev/wp-content/uploads/three.png" alt="Propuesta tres">
+                    </figure>
+                    <div class="mv-propuesta-content">
+                        <h2>Lo aterrizamos juntos y yo le sigo.</h2>
+                        <p>La mejor solución si tu objetivo es crear la mejor experiencia de usuario para tus visitantes, cien por ciento personalizada para atraer y enamorar a tu cliente ideal.</p>
+                    </div>
+                </a>
+            </article>
+        </section>
+    </div>
+<section id="soluciones" class="contenido-propuestas">
+<article id="uno"class="opcion-uno display-uno">
+<h2 class="titulo-propuesta">Hazlo tú solo, yo te enseño.</h2>
+<p>Si eres de los que no tienen miedo de picarle, esta opción es para ti. En esta serie de videos te enseño a:</p>
+<ul>
+  <li>Crear un AMP Stack (Apache, MySQL y PHP) para hospedar, crear y editar un sitio web desde tu computadora, sea MAC o PC.</li>
+  <li>Instalar WordPress para crear un sitio web donde puedas crear tu sitio web más el contenido de este y consejos sobre cómo hacerlo atractivo a tu audiencia.</li>
+  <li>Mantener tu sitio web seguro con una estrategia de actualizaciones y backups que te evite dolores de cabeza.</li>
+  <li>Cómo utilizar y sacar el máximo provecho de tu contenido para atraer a clientes y prospectos interesados en lo que tu haces.</li>
+</ul>
+<p>Estos videos están en producción pero, si estás interesado en aprender, <a href="http://michstart.dev/contacto/">deja tus datos</a> y yo te aviso cuando todo esté listo.</p>
+<p class="disclaimer">Te recomiendo que utilices las mismas herramientas que yo para que puedas seguirme sin complicación mas esto es totalmente opcional. En algunos casos, te dejaré enlaces de productos que utilizo y recomiendo de los cuales soy afiliado, así que si das clic en alguno de esos enlaces y compras a través de estos yo gano una comisión.</p>
+</article>
+<article id="dos" class="opcion-dos display-dos">
+<h2 class="titulo-propuesta">Te ayudo a hacerlo, tú le sigues.</h2>
+<p>Tengo 2 opciones para ayudarte a acelerar el crecimiento de tu negocio.</p>
+<br />
+<div class="one-half first">
+  <h3 class="op-uno">1. Sitio configurado en 24 horas.</h3>
+    <p>Te instalo y configuro el tema de StudioPress de tu elección posterior a la recepción de tu información y pago correspondiente, en menos de 24 horas.</p>
+  <p class="incluye">Incluye:</p>
+  <ul>
+    <li>Instalación de cualquier tema de StudioPress tal cómo está el demo</li>
+    <li>Plugins utilizados en el demo y recomendados para mantener tu sitio seguro</li>
+    <li>Tu logo e información de copyright en el pie de página</li>
+    <li>Acceso a videos instructivos sobre cómo utilizar WordPress</li>
+    <li>Todo listo en 24 horas</li>
+    <br />
+<p class="precio">Inversión: $3,000</p>
+</div>
+  <div class="one-half">
+    <h3>2. Sitio con tu branding en 48 horas.</h3>
+    <p>Te instalo el tema que me pidas de StudioPress y lo dejo tal cómo está el demo pero con tu branding y tu contenido.</p>
+  <p class="incluye">Incluye:</p>
+  <ul>
+    <li>Instalación de cualquier tema de StudioPress</li>
+    <li>Plugins utilizados en el demo y recomendados para mantener tu sitio seguro</li>
+    <li>Colores, logo y tipografía de tu negocio</li>
+    <li>Menu de navegación con la información de tus páginas</li>
+    <li>10 páginas con tu contenido (no creo textos por tí)</li>
+    <li>Acceso a videos instructivos sobre cómo utilizar WordPress</li>
+    <li>Acceso al curso Facebook Marketing 101, para que utilices Facebook para atraer más seguidores y des difusión de lo que haces de la manera correcta</li>
+    <li>Todo listo en 48 horas</li>
+  </ul>
+    <p class="precio">Inversión: $7,000</p>
+    <p class="disclaimer">Estos paquetes no incluyen el Genesis Framework, revisiones ni personalizaciones adicionales a las descritas.</p>
+</div>
+</article>
+<span class="clear-line"></span>
+<article id="tres" class="opcion-tres display-tres">
+<h2 class="titulo-propuesta">Lo&nbsp;aterrizamos juntos y yo le sigo.</h2>
+<p>Creo una página 100% personalizada para transformar tu idea en realidad, con una arquitectura y estructura de información lógica y estratégica para que alcances de manera más rápida lo que quieres conseguir.</p>
+<p class="incluye">Incluye:</p>
+<ul>
+    <li>Investigación de lo que quieres conseguir con tu página, tu cliente o mercado ideal y lo que la página va a resolver para tu negocio.</li>
+    <li>Estrategia y planeación de contenido para optimizar tus conversiones y tráfico de acuerdo a los indicadores clave de rendimiento (KPIs) establecidos.</li>
+    <li>Creación en conjunto de un diseño que transmita tu branding y mensaje con el que te vas a comunicar con tus clientes o prospectos.</li>
+    <li>Desarrollo de página web optimizada, rápida, segura y fácil de utilizar.</li>
+    <li>Area de administración amigable para que puedas autoadministrar tu sitio, con el entrenamiento necesario para que puedas crear, editar y modificar tu contenido fácilmente.</li>
+    <li>Optimización social de página para que el contenido pueda ser compartido de manera sencilla en redes sociales</li>
+    <li>Asesoría básica de inbound para que sepas cómo atraer más clientes y difundas tu mensaje en distintos canales de manera efectiva.</li>
+    <li>Mantenimiento y hosting por un año</li>
+  </ul>
+<br />
+<p class="disclaimer">IMPORTANTE: La inversión más importante es tu tiempo para que podamos crear en conjunto una página web con la estrategia que te ayude a acelerar tu retorno de inversión</p>
+<p class="precio">Inversión: A partir de $24,000</p>
+</article>
+</section>
+
+<?php
+}
+
+genesis();
